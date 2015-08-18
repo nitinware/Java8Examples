2115048811e4b9f9e36e6b6f7f81d4d0bce0c90c
nitinware <nitinware@gmail.com>
2015-08-17 22:13:45 -0400

    Runnable example a TimeUnit Delay.

--- src/com/java8/concurrency/examples/RunnableDemo.java
+++ src/com/java8/concurrency/examples/RunnableDemo.java
@@ -1,12 +1,20 @@
 package com.java8.concurrency.examples;
 
+import java.util.concurrent.TimeUnit;
+
 public class RunnableDemo {
 
 	public static void main(String[] args) {
 		
 		Runnable task = () -> {
-			String threadName = Thread.currentThread().getName();
-			System.out.println("Hello " + threadName);
+			try {
+				String threadName = Thread.currentThread().getName();
+				System.out.println("Foo " + threadName);
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println("Bar " + threadName);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			};
 		
 		task.run();
